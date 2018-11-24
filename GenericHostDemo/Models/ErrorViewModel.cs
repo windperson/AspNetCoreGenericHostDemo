72c6c5d5afea748717a296420e36c10699e85bbf
Chen Yu Pao <windperson@gmail.com>
2018-11-24 17:31:44 +0800

    Add project files.

---
+++ GenericHostDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GenericHostDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
